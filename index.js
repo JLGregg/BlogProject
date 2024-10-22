d5cac48fd7f83ffe1e3c6f76c8b8ca9f664f30a9
JLGregg <banjer1982@yahoo.com>
2024-10-21 21:06:47 -0400

    added navigation. linked css

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    // TODO: add res.render for ejs
-    res.send("home");
+    res.render("index.ejs");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
 })
 
 app.get("/view", (req, res) => {
