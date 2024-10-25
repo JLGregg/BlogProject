7298ae57b37d97dcb91ccad08d6e3600f011826d
Jason Gregg <38503403+JLGregg@users.noreply.github.com>
2024-10-25 10:47:26 -0400

    create link to each post in index.ejs. added viewpost.ejs to view posts.

--- index.js
+++ index.js
@@ -13,27 +13,33 @@ app.use(express.urlencoded({ extended:true }));
 // Get home page
 app.get("/", (req, res) => {
     res.render("index.ejs", { blogTitles: blogTitles});
-})
+});
 
 // Get contact page
 app.get("/contact", (req, res) => {
     res.render("contact.ejs");
-})
+});
 
 // Get about page
 app.get("/about", (req, res) => {
     res.render("about.ejs");
-})
+});
 
-app.get("/viewPost", (req, res) => {
-    // TODO: add correct code
-    res.send("view");
-})
+app.get("/viewPost/:id", (req, res) => {
+    const postId = req.params.id;
+    const post = blogTitles.find(p => p.id === postId);
+
+    if (post) {
+        res.render("viewPost.ejs", {post});
+    } else {
+        res.status(404).send("Post not found");
+    }
+});
 
 // Get page to fill out form
 app.get("/submit", (req, res) => {
     res.render("entry.ejs");
-})
+});
 
 // Post form to /submit
 app.post("/submit", (req, res) => {
@@ -45,22 +51,22 @@ app.post("/submit", (req, res) => {
     };
 
     // Push blogTitle to blogTitles list
-    blogTitles.push(blogTitle);
+    blogTitles.push(blogPost);
 
     // Redirect back to home page
     res.redirect("/");
-})
+});
 
 app.patch("/edit", (req, res) => {
     // TODO: add correct code
     res.send("edit");
-})
+});
 
 app.delete("/delete", (req, res) => {
     // TODO: add correct code
     res.send("delete");
-})
+});
 
 app.listen(port, () => {
     console.log(`You are listening to port: ${port}`);
-})
+});
